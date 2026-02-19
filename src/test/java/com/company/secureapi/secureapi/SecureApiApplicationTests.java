962425f68b8aca8a08354494284c5f8f536c8718
chandrashekar975 <rchandrashekarreddy19@gmail.com>
2026-02-19 22:48:45 +0530

    Initial Spring Boot Project Setup with dependencies and clean structure

---
+++ src/test/java/com/company/secureapi/secureapi/SecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.secureapi.secureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
